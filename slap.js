4146b44cd9b65c8f54fa99943fb945af26c174c3
hongyaohurhurhur <tedtest65@gmail.com>
2026-03-12 00:05:33 +0800

    Add whack a mole game

---
+++ slap.js
@@ -0,0 +1,59 @@
+var hits = 0;
+var gameOver = false;
+var count = 30;
+var counter = setInterval(timer, 1000);
+setTimeout("pop()", 3000);
+
+let images = [
+    "punchimages/miku.png",
+    "punchimages/miku_lightdmg1.png",
+    "punchimages/miku_lightdmg2.png",
+    "punchimages/miku_lightdmg3.png",
+    "punchimages/miku_dmg1.png",
+    "punchimages/miku_dmg2.png",
+    "punchimages/miku_dmg3.png",
+    "punchimages/miku_dmg4.png",
+    "punchimages/miku_dmg5.png",
+];
+
+function pop() {
+  var x = Math.random() * 500;
+  var y = Math.random() * 150;
+  $(".mole").css("opacity", 1);
+  $(".mole").css({
+    top: y,
+    left: x
+  });
+  setTimeout("pop()", Math.random() * 3000);
+}
+
+let index = 0;
+$('.mole').click(function() {
+  if (gameOver) return;
+  $('#sound').html("<audio autoplay><source src='sounds/punch.wav' type='audio/mpeg'>");
+  $(".mole").css("opacity", 0);
+  $("#score").html(++hits);
+  $(this).css("background-image", "url('" + images[index] + "')");
+  index++;
+  if (index == images.length) {
+        gameOver = true;
+        $("#timer").html("不要再打了");
+    }
+
+});
+
+$('button').click(function() {
+  count = 30;
+  hits = 0;
+  $("#score").html(hits);
+  gameOver = false;
+});
+
+function timer() {
+  if (count <= 0) {
+    gameOver = true;
+    $("#timer").html("Game&nbsp;Over");
+    return;
+  }
+  $("#timer").html((count<10?"0:0":"0:")+count--);
+}
